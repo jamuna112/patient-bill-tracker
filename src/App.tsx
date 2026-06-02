06280d29c5df9de0f95321211f683b383357fd2e
jamuna subedi <jamunaadhikari112@gmail.com>
2026-06-02 14:07:46 -0700

    fix: bill card updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import BillCard from "./components/BillCard"
 import { patients } from "./app/mocks/patients"
 import { useState } from "react"
+import { Search } from "lucide-react"
 
 
 const todaysDate = new Date().toLocaleDateString("en-US", {
@@ -28,12 +29,13 @@ const patientsWithStatus = patients.map((patient) => ({
 
 function App() {
   const [activeFilter, setActiveFilter] = useState("All")
+  const [searchTerm, setSearchTerm] = useState("")
 
   const filteredPatients = patientsWithStatus.filter((patient) => {
-    if (activeFilter === "All") {
+    if (activeFilter === "All" && (patient.fullName.toLowerCase().includes(searchTerm.toLowerCase()))) {
       return true
     }
-    return patient.status === activeFilter
+    return patient.status === activeFilter && (patient.fullName.toLowerCase().includes(searchTerm.toLowerCase()))
   })
 
   return (
@@ -43,21 +45,30 @@ function App() {
       <h1 className="text-center font-extrabold text-3xl">Patient Billing Tracker</h1>
       <p className="text-center font-light text-md">A simple, secure app to track patient invoices, manage healthcare payments, and monitor outstanding medical balances in one place.</p>
       <p className="text-md text-center">{todaysDate}</p>
-      <div>
-        <button className={`${activeFilter === "All" ? "bg-blue-500" : "bg-blue-950"} rounded-2xl h-8 w-25 m-2 text-white  active:scale-95`} onClick={() => {
-          setActiveFilter("All")
-        }}>All</button>
-        <button className={` ${activeFilter === "Paid" ? "bg-green-500" : "bg-blue-950"} rounded-2xl h-8 w-25 m-2 text-white active:scale-95`} onClick={() => {
-          setActiveFilter("Paid")
-        }}>Paid</button>
-        <button className={`${activeFilter === "Unpaid" ? "bg-red-500" : "bg-blue-950"} rounded-2xl h-8 w-25 m-2 text-white active:scale-95`} onClick={() => {
-          setActiveFilter("Unpaid")
-        }}>Unpaid</button>
-        <button className={`${activeFilter === "Pending" ? "bg-yellow-500" : "bg-blue-950"} rounded-2xl h-8 w-25 m-2 text-white active:scale-95`} onClick={() => {
-          setActiveFilter("Pending")
-        }}>Pending</button>
+      <div className="flex justify-between">
+        <div className="relative w-70">
+          <Search className="absolute right-3 top-1/2 py-0.5 -translate-y-1/2 pointer-events-none" />
+          <input type="text" placeholder="Search" className="bg-amber-200 h-8 w-70 m-2 rounded-2xl text-sm px-2 " onChange={(e) => {
+            setSearchTerm(e.target.value)
+          }} />
+        </div>
+
+        <div>
+          <button className={`${activeFilter === "All" ? "bg-blue-500" : "bg-blue-950"} rounded-2xl h-8 w-25 m-2 text-white  active:scale-95`} onClick={() => {
+            setActiveFilter("All")
+          }}>All</button>
+          <button className={` ${activeFilter === "Paid" ? "bg-green-500" : "bg-blue-950"} rounded-2xl h-8 w-25 m-2 text-white active:scale-95`} onClick={() => {
+            setActiveFilter("Paid")
+          }}>Paid</button>
+          <button className={`${activeFilter === "Unpaid" ? "bg-red-500" : "bg-blue-950"} rounded-2xl h-8 w-25 m-2 text-white active:scale-95`} onClick={() => {
+            setActiveFilter("Unpaid")
+          }}>Unpaid</button>
+          <button className={`${activeFilter === "Pending" ? "bg-yellow-500" : "bg-blue-950"} rounded-2xl h-8 w-25 m-2 text-white active:scale-95`} onClick={() => {
+            setActiveFilter("Pending")
+          }}>Pending</button>
+        </div>
       </div>
-      <div className="grid grid-cols-3 gap-2 m-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-2 gap-6">
         {
           filteredPatients.map((patient) => {
             return (
--- src/components/BillCard.tsx
+++ src/components/BillCard.tsx
@@ -11,23 +11,42 @@ function BillCard({ bill }) {
   };
 
   return (
-    <div className=" p-4 rounded-lg shadow-md mb-4 inline-block bg-gray-800">
-      <h3 className="text-white">{bill.fullName}</h3>
+    <div className=" p-4 rounded-lg shadow-md bg-gray-800">
+      <h1 className="text-white text-3xl text-center">{bill.fullName}</h1>
       <dl>
-        <dt>Hospital: </dt>
-        <dd>{bill.hospitalProvider}</dd>
-        <dt>Total Bill:</dt>
-        <dd>{formatter.format(bill.totalBillAmount)}</dd>
-        <dt>Description:</dt>
-        <dd>{bill.description}</dd>
-        <dt>Status:</dt>
-        <dd
-          className={`${statusColors[bill.status]} inline-block px-3 py-1 rounded-full`}
-        >
-          {bill.status}
-        </dd>
-        <dt>Remaining Balance:</dt>
-        <dd>{formatter.format(bill.remainingAmount)}</dd>
+        <div className="space-y-2 mt-4">
+          <div className="flex justify-between">
+            <span>Hospital</span>
+            <span>{bill.hospitalProvider}</span>
+          </div>
+        </div>
+        <div className="space-y-2 mt-4">
+          <div className="flex justify-between">
+            <span>Total Bill:</span>
+            <span>{formatter.format(bill.totalBillAmount)}</span>
+          </div>
+        </div>
+
+        <div className="space-y-2 mt-4">
+          <div className="flex justify-between">
+            <span>Status: </span>
+            <span className={`${statusColors[bill.status]} inline-block px-3 py-1 rounded-full`}>{bill.status}</span>
+          </div>
+        </div>
+
+        <div className="space-y-2 mt-4">
+          <div className="flex justify-between">
+            <span>Remaining Balance: </span>
+            <span>{formatter.format(bill.remainingAmount)}</span>
+          </div>
+        </div>
+
+        <div className="space-y-2 mt-4">
+          <div className="flex justify-between">
+            <span>Description: </span>
+            <span>{bill.description}</span>
+          </div>
+        </div>
       </dl>
     </div>
   );
